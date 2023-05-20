dbd91416e7e30b37a2f668c5e60f1afc0da1a1b8
Lucas Siani <70410261+Lucas199525@users.noreply.github.com>
2023-05-20 21:42:55 +0200

    add user slice

--- src/store/listeUsers/ListeUsersSlice.ts
+++ src/store/listeUsers/ListeUsersSlice.ts
@@ -1,1 +1,28 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface IList {
+  TUsers: any;
+}
+
+const getInitialState = () => {
+  return {
+    value: {
+      TUsers: [],
+    },
+  };
+};
+
+const UsersSlice = createSlice({
+  name: 'Users',
+  initialState: getInitialState(),
+  reducers: {
+    updateUsers(state, action: PayloadAction<IList>) {
+      state.value.TUsers = action.payload.TUsers;
+    },
+  },
+});
+
+export const { updateUsers } = UsersSlice.actions;
+export const selectUser = (state: { User: { value: IList } }) =>
+  state.User.value;
+export default UsersSlice;
