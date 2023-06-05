ade3c80ca08bd1fa5f6d8b033517c9afd2a06973
Lucas Siani <70410261+Lucas199525@users.noreply.github.com>
2023-06-05 02:12:38 +0200

    maj

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import './contact.css';
+import { BsWhatsapp, BsMailbox2, BsFacebook } from 'react-icons/bs';
 
 const Contact = () => {
   return (
     <section id="contact">
-      <p>Contact</p>
+      <h5>Get In Touch</h5>
+      <h2>Me Contacter</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <BsMailbox2 className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>lucasfauresiani@gmail.com</h5>
+            <a href="">Envoyer</a>
+          </article>
+          <article className="contact__option">
+            <BsFacebook className="contact__option-icon" />
+            <h4>Messanger</h4>
+            <h5>lucas Siani</h5>
+            <a href="">Envoyer</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>0614819048</h5>
+            <a href="">Envoyer</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name="name" placeholder="Nom, Prenom" required />
+          <input type="text" name="mail" placeholder="Mail" required />
+          <textarea name="message" placeholder="Message"></textarea>
+          <button type="submit" className="btn btn-primary">
+            Envoyer
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './experience.css';
 import { BsPatchCheckFill } from 'react-icons/bs';
 
 const Experience = () => {
@@ -12,77 +13,101 @@ const Experience = () => {
           <div className="experience__content">
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>Type Script, Java Script</h4>
-              <small></small>
+              <div>
+                <h4>Type Script, Java Script</h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>React.js</h4>
-              <small></small>
+              <div>
+                <h4>React.js</h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>Java</h4>
-              <small></small>
+              <div>
+                <h4>Java</h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>Spring Boot, Swagger</h4>
-              <small></small>
+              <div>
+                <h4>Spring Boot, Swagger</h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>PostgreSQL</h4>
-              <small></small>
+              <div>
+                <h4>PostgreSQL</h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>SQLServer</h4>
-              <small></small>
+              <div>
+                <h4>SQLServer</h4>
+                <small></small>
+              </div>
             </article>
           </div>
         </div>
         <div className="experience__backend">
           <h3>Systemes et reseaux</h3>
-          <div className="experience__content">
+          <div className="experience__content__tech">
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>
-                Dépannage utilisateur: software, hardware et système
-                d’exploitation{' '}
-              </h4>
-              <small></small>
+              <div>
+                <h4>
+                  Dépannage utilisateur: software, hardware et système
+                  d’exploitation
+                </h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>Gestion des comptes dans L’active directory</h4>
-              <small></small>
+              <div>
+                <h4>Gestion des comptes dans L’active directory</h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>Gestion des connexions aux serveurs</h4>
-              <small></small>
+              <div>
+                <h4>Gestion des connexions aux serveurs</h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>
-                Système d’exploitation : Windows 10, 8,7 Windows server / linux
-                Debian, Ubuntu, kali, Parrot, PopOS
-              </h4>
-              <small></small>
+              <div>
+                <h4>
+                  Système d’exploitation : Windows 10, 8,7 Windows server /
+                  linux Debian, Ubuntu, kali, Parrot, PopOS
+                </h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>
-                Gestion des connexions au réseau, Mise en place de server de
-                virtualisation Proxmox
-              </h4>
-              <small></small>
+              <div>
+                <h4>
+                  Gestion des connexions au réseau, Mise en place de server de
+                  virtualisation Proxmox
+                </h4>
+                <small></small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>Cybersécurité</h4>
-              <small></small>
+              <div>
+                <h4>Cybersécurité</h4>
+                <small></small>
+              </div>
             </article>
           </div>
         </div>
